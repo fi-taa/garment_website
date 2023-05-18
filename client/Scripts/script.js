0e35422c87ab4ce9dc4954cf7899b149be8f5a73
Fita-Wegene <fitaawd@gmail.com>
2023-05-18 20:13:06 +0300

    garment client side first commit

---
+++ client/Scripts/script.js
@@ -0,0 +1,32 @@
+// JavaScript code for product.html
+
+// Smooth scrolling for cloth type links
+document.querySelectorAll('.cloth-types ul li a').forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+  
+      // Remove the active class from all links
+      document.querySelectorAll('.cloth-types ul li a').forEach((el) => {
+        el.classList.remove('active');
+      });
+  
+      // Add the active class to the clicked link
+      link.classList.add('active');
+  
+      // Get the target section id from the link's href attribute
+      const targetId = link.getAttribute('href').substring(1);
+  
+      // Show the target section and hide the others
+      document.querySelectorAll('.product-section').forEach((section) => {
+        if (section.id === targetId) {
+          section.style.display = 'block';
+        } else {
+          section.style.display = 'none';
+        }
+      });
+  
+      const target = document.querySelector(link.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
